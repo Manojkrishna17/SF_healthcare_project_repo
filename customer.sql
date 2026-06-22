d7e84f5e9fd89942d116229bf8ee4c6a1023a7c2
ManojKrishna T <immanojmanu1996@gmail.com>
2026-06-22 22:21:17 +0530

    Create customer.sql

---
+++ customer.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TABLE CUSTOMER (
+    CUSTOMER_ID NUMBER,
+    CUSTOMER_NAME STRING,
+    CITY STRING,
+    AGE NUMBER
+);
+
+INSERT INTO CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME, CITY, AGE)
+VALUES
+(1, 'John', 'New York', 25),
+(2, 'Emma', 'Chicago', 30),
+(3, 'Michael', 'Dallas', 28),
+(4, 'Sophia', 'Seattle', 32),
+(5, 'William', 'Miami', 27);
